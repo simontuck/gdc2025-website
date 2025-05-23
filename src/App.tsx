75301d0f5ff7216a9defb20297937cb551f65cd0
Simon Tuck <simontuck@users.noreply.github.com>
2025-05-23 11:08:22 +0200

    Fix Router context error

--- src/App.tsx
+++ src/App.tsx
@@ -63,4 +63,12 @@ const AppContent = () => {
   );
 };
 
-export default AppContent
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+};
+
+export default App;
