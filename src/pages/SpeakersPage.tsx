f8b99dc4a7d40cb586a634afeaa1496f4284c235
Simon Tuck <simontuck@users.noreply.github.com>
2025-06-04 21:54:51 +0200

    Deploy application to Netlify

--- src/pages/SpeakersPage.tsx
+++ src/pages/SpeakersPage.tsx
@@ -28,7 +28,7 @@ const SpeakerModal: React.FC<SpeakerModalProps> = ({ speaker, isOpen, onClose })
   return (
     <div className="fixed inset-0 z-50 overflow-y-auto">
       <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
-        <div className="fixed inset-0 transition-opacity" onClick={onClose}>
+        <div className="fixed inset-0 transition-opacity\" onClick={onClose}>
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
 
